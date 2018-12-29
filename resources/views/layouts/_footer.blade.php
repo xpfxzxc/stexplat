52c1a425639b5666d74d94c8c9b52402296b26d8
xpfxzxc <xpfxzxc@gmail.com>
2018-12-29 08:55:26 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Copyright © 2018 winy1909. All Rights Reserved.
+    </p>
+
+    <p class="float-right"><a href="mailto:winy1909@qq.com">联系我们</a></p>
+  </div>
+</footer>
