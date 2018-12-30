54ac20fa875da105ec6598e62668eb56b39dada1
xpfxzxc <xpfxzxc@gmail.com>
2018-12-30 07:48:44 +0000

    用户数据模型

---
+++ database/migrations/2018_12_30_144721_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sex');
+            $table->integer('age');
+            $table->string('education');
+            $table->string('address');
+            $table->integer('cell_phone');
+            $table->integer('user_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
